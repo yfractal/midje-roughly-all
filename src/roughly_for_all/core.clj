1d479ece09a422f3f9e2b9362d523c60e0802d65
yfractal <mike@beansmile.com>
2014-04-28 14:46:29 +0800

    roughly-all for number and coll

--- src/roughly_for_all/core.clj
+++ src/roughly_for_all/core.clj
@@ -1,1 +1,40 @@
-(ns roughly-for-all.core)
+(ns roughly-for-all.core
+  (use [midje.checking.checkers.defining :only [checker defchecker]]))
+
+(def TOLERANCE  1.0E-6)
+(def roughly-equal?)
+
+(defn number-roughly-equal?
+  [expected actual tolerance]
+  (and (>= expected (-' actual tolerance))
+       (<= expected (+' actual tolerance))))
+
+(defn coll-roughly-equal?
+  [compare-val compared-val tolerance]
+  (loop [remain-compare-val compare-val
+         remain-compared-val compared-val
+         eq true]
+    (cond (not eq) false
+          (empty? remain-compare-val) true
+          :else (recur
+                 (rest remain-compare-val)
+                 (rest remain-compared-val)
+                 (roughly-equal? (first remain-compare-val) (first remain-compared-val) tolerance)))))
+
+(defn comparable?
+  [a1 a2]
+  (or (=  (type a1) (type a2))
+      (and (number? a1) (number? a2))))
+
+(defn roughly-equal?
+  [expected actual tolerance]
+  (cond (not (comparable? expected actual)) false
+        (number? expected) (number-roughly-equal? expected actual tolerance)
+        (coll? expected) (coll-roughly-equal? expected actual tolerance)))
+
+(defchecker roughly-all
+  ([expected tolerance]
+     (checker [actual]
+              (roughly-equal? expected actual tolerance)))
+  ([expected]
+     (roughly-all expected TOLERANCE)))
--- test/roughly_for_all/core_test.clj
+++ test/roughly_for_all/core_test.clj
@@ -1,3 +1,46 @@
 (ns roughly-for-all.core-test
   (:require [clojure.test :refer :all]
-            [roughly-for-all.core :refer :all]))
+            [roughly-for-all.core :refer :all])
+  (:use midje.sweet
+      [midje.checking.checkers.defining :only [checker?]]))
+
+(def HALF-TOLERANCE  (/ 1.0E-6 2))
+(def DOUBLE-TOLERANCE  (* 1.0E-6 2))
+
+(facts "number-roughly-equal?"
+       (number-roughly-equal? 1 (+ 1 0.5) 1) => true
+       (number-roughly-equal? 1 (+ 1 2.0) 1) => false
+
+       (number-roughly-equal? 1 (- 1 0.5) 1) => true
+       (number-roughly-equal? 1 (- 1 2.0) 1) => false)
+
+(facts "coll-roughly-equal?"
+       (fact "coll"
+             (coll-roughly-equal? '(1 2) '(1.5 2.5) 1) => true
+             (coll-roughly-equal? '(1 2) '(1.5 2.5) 0.4) => false
+             (coll-roughly-equal? [1 2] [1.5 2.5] 0.4) => false)
+       (fact "recursive coll"
+             (coll-roughly-equal? (list 1 (list 2)) (list 1.5 (list 2.5)) 1) => true
+             (coll-roughly-equal? (list 1 (list 2)) (list 1.5 (list 2.5)) 0.4) => false))
+
+(facts "roughly-equal? a dispatch by input type"
+       (fact "different type, should return false"
+             (roughly-equal? 1 [1] 3) => false)
+       (fact "compare number by number-roughly-equal?"
+             (roughly-equal? 1 2 3) => anything
+             (provided
+              (number-roughly-equal? 1 2 3) => anything :times 1))
+       (fact "it should call coll-roughly-equal?"
+             (roughly-equal? (list 1) (list 1.5) 1) => true
+             (provided
+              (coll-roughly-equal? anything anything anything) => true :times 1)))
+
+(facts "roughly-all"
+       (fact "it is a checker"
+             #'roughly-all => checker?
+             roughly-all => checker?
+             (roughly-all 3) => checker?
+             (roughly-all 3 1) => checker?)
+       (fact "for number"
+             (+ 1 HALF-TOLERANCE) => (roughly-all 1)
+             1.5 => (roughly-all 1 1)))
